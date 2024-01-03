6e495a8540e3815b529dc765eb76e4b91fd97e41
Wonders Victor <wondersprince1@gmail.com>
2024-01-03 17:18:20 +0100

    Rotations done

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
- * 
- * 
- * 
- * 
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: is a pointer to the first node
+ * @second: is a pointer to the second node
+ *
+ * Return: Lowest common ancestor
 */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+										const binary_tree_t *second)
 {
 	binary_tree_t *hare = NULL, *tortoise = NULL;
 
--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -2,32 +2,27 @@
 #include <sys/types.h>
 
 /**
+ *binary_tree_rotate_left - performs a left-rotation on a binary tree
+ *@tree: is a pointer to the root node of the tree to rotate
  *
  *
- *
- *
- *
- *
- *
+ *Return: pointer to the new root node of the tree once rotated.
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *temp = NULL, *newRoot = NULL;
 
-	if (!tree)
+	if (!tree || !tree->right)
 		return NULL;
 
-	if (tree->right)
-	{
-		newRoot = tree->right;
-		tree->parent = newRoot;
-		temp = newRoot->left;
-		newRoot->left = tree;
-		tree->right = temp;
-		temp->parent = temp;
-
-		return newRoot;
-	}
-
-	return tree;
+	newRoot = tree->right;
+	temp = newRoot->left;
+	tree->right = newRoot->left;
+	if (temp)
+		temp->parent = tree;
+	newRoot->left = tree;
+	newRoot->parent = tree->parent;
+	tree->parent = newRoot;
+	
+	return newRoot;
 }
